9426af2c7e1f3951214826f0a36a4f9296a78a79
yahayagodiwa <yahayagodiwa8@gmail.com>
2025-10-05 10:44:04 +0100

    dfg

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:interactives/widgets/expenses.dart';
-
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.from(alpha: 255, red: 96, green: 59, blue: 181));
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      colorScheme: kColorScheme,
+    ),
     home: Expenses(),
   ));
 }
